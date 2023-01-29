d20b7126de54d40b127715a9408893b27cbf66f8
Ryan Kennedy <ryan.kennedy@droplet.io>
2023-01-30 09:42:17 +1000

    feat: initial version

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,60 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const SEPARATOR_REGEX = /^[\s]*\/\/[\s]*---+$/;
+const HEADING_REGEX = /^[\s]*\/\/[\s]*(.*)[\s]*$/;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'tidy-comments.tidyOne',
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tidy-comments" is now active!');
+      // Get the text on the previous line, current line, and next line.
+      const previousLine: null | vscode.TextLine =
+        editor.selection.active.line === 0
+          ? null
+          : editor.document.lineAt(editor.selection.active.line - 1);
+      const currentLine = editor.document.lineAt(editor.selection.active.line);
+      const isLastLine =
+        editor.selection.active.line === editor.document.lineCount - 1;
+      const nextLine = isLastLine
+        ? null
+        : editor.document.lineAt(editor.selection.active.line + 1);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tidy-comments.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Tidy Comments!');
-	});
+      const currentLineMatch = HEADING_REGEX.exec(currentLine.text);
+      if (!currentLineMatch) {
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      // Generate a list of dashes the same length as the current line's heading
+      const dashes = '-'.repeat(currentLineMatch[0].length - 3);
+
+      editor.edit((editBuilder) => {
+        if (previousLine && SEPARATOR_REGEX.test(previousLine.text)) {
+          editBuilder.replace(previousLine.range, `// ${dashes}`);
+        } else {
+          editBuilder.insert(currentLine.range.start, `// ${dashes}\n`);
+        }
+
+        if (nextLine && SEPARATOR_REGEX.test(nextLine.text)) {
+          editBuilder.replace(nextLine.range, `// ${dashes}`);
+        } else {
+          editBuilder.insert(currentLine.range.end, `\n// ${dashes}`);
+        }
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
